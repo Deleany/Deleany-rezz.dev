0819ef8d7a04cc8111ddd4495e8c5dd865dccde3
rezz <artofexe@gmail.com>
2022-04-24 13:43:15 +0000

    fix script

--- tests.sh
+++ tests.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 echo "======================TEST STARTED======================"
 sleep 5
 result=`grep "Hello" index.html | wc -l`
-if [ $result = 1 ]
+if [ "$result" = "1" ]
 then
 	echo "Test Passed"
 else
